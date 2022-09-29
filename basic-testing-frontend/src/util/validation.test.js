50cf9300b738c02a9655bb63632ccdbf8e3389c9
Achref <medachrefg@gmail.com>
2022-09-29 16:14:29 +0100

    test suits using describe()

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,20 +1,24 @@
-import { expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { validateNumber, validateStringNotEmpty } from './validation';
 
-it('should throw and error if value not a string  ', () => {
-	const input = {};
-	const input2 = 1;
-	const resultFn = () => {
-		validateStringNotEmpty(input);
-		validateStringNotEmpty(input2);
-	};
-	expect(resultFn).toThrow();
+describe('validateStringNotEmpty()', () => {
+	it('should throw and error if value not a string  ', () => {
+		const input = {};
+		const input2 = 1;
+		const resultFn = () => {
+			validateStringNotEmpty(input);
+			validateStringNotEmpty(input2);
+		};
+		expect(resultFn).toThrow();
+	});
 });
 
-it('should throw an error if a non numeric value is provided ', () => {
-	const input = '1';
-	const validatFn = () => {
-		validateNumber(input);
-	};
-	expect(validatFn).toThrow();
+describe('validateNumber()', () => {
+	it('should throw an error if a non numeric value is provided ', () => {
+		const input = '1';
+		const validatFn = () => {
+			validateNumber(input);
+		};
+		expect(validatFn).toThrow();
+	});
 });

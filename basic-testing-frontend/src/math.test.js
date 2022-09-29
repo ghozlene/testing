c48f4914379f80902b82da48fcfae531c8e445f1
Achref <medachrefg@gmail.com>
2022-09-29 12:32:52 +0100

    adding more test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -7,8 +7,30 @@ it('should summarize all number in an array ', () => {
 	//Act
 	const result = add(numbers);
 	//Assert
-	const expectResult = numbers.reduce((prevValue, curValue) => {
-		prevValue + curValue;
-	}, 0);
+	const expectResult = numbers.reduce(
+		(prevValue, curValue) => prevValue + curValue,
+		0
+	);
+	expect(result).toBe(expectResult);
+});
+
+it('should yield NaN if at lease one invalid number is provided ', () => {
+	//	Arrange
+	const inputs = ['invalid', 1];
+
+	const result = add(inputs);
+
+	expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided ', () => {
+	const inputs = ['1', '2', '3'];
+
+	const result = add(inputs);
+
+	const expectResult = inputs.reduce(
+		(prevValue, curValue) => +prevValue + +curValue,
+		0
+	);
 	expect(result).toBe(expectResult);
 });

00296002c37b5c6a5c09679b6207513142210a0b
Achref <medachrefg@gmail.com>
2022-10-03 22:20:11 +0100

    Testing Async code with callbacks(done) passing done

--- basic-testing-frontend/src/async/async.test.js
+++ basic-testing-frontend/src/async/async.test.js
@@ -2,11 +2,18 @@ import { describe, expect, it } from 'vitest';
 import { generateToken } from './async';
 
 describe('generateToken()', () => {
-	it('should return a token based on the email giving', () => {
+	it('should return a token based on the email giving', (done) => {
 		const testUserEmail = 'aaaa@aaa.com';
 
 		generateToken(testUserEmail, (err, token) => {
-			expect(token).toBeDefined();
+			//
+			try {
+				//expect(token).toBe(1);
+				expect(token).toBeDefined();
+				done();
+			} catch (error) {
+				done(error);
+			}
 		});
 	});
 });
